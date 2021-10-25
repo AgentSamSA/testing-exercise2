31d6eb229a4ecc6b15e58ed84217e4ccd7ffd937
Samuel Lee <samuelxlee@gmail.com>
2021-10-25 14:30:53 -0700

    added config and wrote application to run

---
+++ src/main/java/com/example/exercise2/AppConfig.java
@@ -0,0 +1,28 @@
+package com.example.exercise2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    
+    @Bean(name = "iCharToNum")
+    public ICharToNum createCharToNum() {
+        return new CharToNum();
+    }
+
+    @Bean(name = "iMultiplyByTwo")
+    public IMultiplyByTwo createMultiplyByTwo() {
+        return new MultiplyByTwo();
+    }
+
+    @Bean(name = "iNumToChar")
+    public INumToChar createNumToChar() {
+        return new NumToChar();
+    }
+
+    @Bean(name = "iFlow")
+        public IFlow createFlow() {
+            return new Flow(createCharToNum(), createMultiplyByTwo(), createNumToChar());
+    }
+}
--- src/main/java/com/example/exercise2/Exercise2Application.java
+++ src/main/java/com/example/exercise2/Exercise2Application.java
@@ -1,13 +1,26 @@
 package com.example.exercise2;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Scanner;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class Exercise2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Exercise2Application.class, args);
+		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		IFlow IF = context.getBean("iFlow", Flow.class);
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter your message: ");
+		String message = s.nextLine();
+		String result = "";
+		for (int i = 0; i < message.length(); i++) {
+			result += IF.getResult(String.valueOf(message.charAt(i)));
+		}
+		System.out.println(result);
+		s.close();
 	}
 
 }
